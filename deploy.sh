12f301916db9cb085d2886ac6bc67a6b93a3a30c
Prathap <prathap.chilukuri@dominodatalab.com>
2019-06-18 08:56:47 +0530

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+docker build -t pratsoff/multi-client:latest -t pratsoff/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t pratsoff/multi-server:latest -t pratsoff/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t pratsoff/multi-worker:latest -t pratsoff/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push pratsoff/multi-client:latest
+docker push pratsoff/multi-server:latest
+docker push pratsoff/multi-worker:latest
+
+docker push pratsoff/multi-client:$SHA
+docker push pratsoff/multi-server:$SHA
+docker push pratsoff/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=pratsoff/multi-server:$SHA
+kubectl set image deployments/client-deployment server=pratsoff/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=pratsoff/multi-worker:$SHA
+
